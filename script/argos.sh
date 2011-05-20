15f29bbc0e031eb8769209643d38cfbc3e81018d
Stuart Sierra <mail@stuartsierra.com>
2011-05-20 12:08:40 -0400

    Add default list of projects

---
+++ script/argos.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`/..
+source script/functions.sh
+
+function print_usage {
+    echo <<EOF
+Usage: $0 [options]
+Options:
+ -o       Offline mode; do not update dependencies
+ -j file  Use file as the clojure.jar
+EOF
+}
+
+DEFAULT_PROJECTS=$(cat projects.txt)
+
+while getopts ":o:p:" opt; do
+    case $opt in
+        o)
+            echo "OFFLINE MODE"
+            OFFLINE="true"
+            ;;
+        j)
+            CLOJURE_JAR="$OPTARG"
+            ;;
+        p)
+            PROJECTS="$PROJECTS $OPTARG"
+            ;;
+        h)
+            print_usage
+            exit 1
+            ;;
+        \?)
+            die "Invalid option: -$OPTARG"
+            ;;
+    esac
+done
+
+if [ -z "$PROJECTS" ]; then
+    PROJECTS="$DEFAULT_PROJECTS"
+fi
+
+mkdir -p "$WORK_DIR"
+
+project_count=0
+failure_count=0
+failed_projects=""
+
+for project in $PROJECTS; do
+    project_count=$(( $project_count + 1 ))
+    echo -e "\n\n============================================================"
+    clone_project "$project"
+    do_argos_project "$project"
+    if [ "$?" != "0" ]; then
+        failure_count=$(( $failure_count + 1 ))
+        failed_projects="$failed_projects $project"
+    fi
+done
+
+echo -e "\n\n============================================================"
+echo "SUMMARY"
+echo "Tested $project_count projects"
+echo "$failure_count failures"
+
+for project in $failed_projects; do
+    echo "    $project"
+done
--- script/functions.sh
+++ script/functions.sh
@@ -1,51 +1,99 @@
 #!/usr/bin/env bash
 
-NETWORK="true"
+OFFLINE="false"
 WORK_DIR="./work"
 
+function die {
+    echo "$@"
+    exit 1
+}
+
 function echodo {
+    local cwd=$(pwd)
+    echo "In $cwd"
     echo "$@"
     "$@"
-    if [ "$?" != "0" ]; then
-        echo "ERROR: return value $?"
+    local rv="$?"
+    if [ "$rv" != "0" ]; then
+        echo "ERROR: exit status $? from '$@'"
     fi
+    return $rv
 }
 
 function clone_project {
-    local username="$1"
-    local project="$2"
+    local project="$1"
+    local userdir=$(dirname $project)
+    local my_dir="$WORK_DIR/$userdir"
+    mkdir -p "$my_dir"
     (
-        cd "$WORK_DIR"
+        cd "$my_dir"
         if [ ! -d "$project" ]; then
-            echodo git clone "git://github.com/${username}/${project}.git"
+            echodo git clone "git://github.com/${project}.git"
         fi
     )
 }
 
-function update_git_in_project {
-    git checkout master
-    git pull
+function update_git {
+    local project="$1"
+    (
+        cd "$WORK_DIR/$project"
+        if [ "$OFFLINE" = "false" ]; then
+            git checkout -q master
+            git pull -q
+        fi
+    )
 }
 
-function update_deps_in_project {
-    if [ -e project.clj -a "$NETWORK" = "true" ]; then
-        echodo lein clean
-        echodo lein deps
-    fi
+function update_deps {
+    local project="$1"
+    (
+        cd "$WORK_DIR/$project"
+        if [ -e project.clj -a "$OFFLINE" = "false" ]; then
+            echodo lein clean
+            echodo lein deps
+        fi
+    )
 }
 
-function run_tests_in_project {
-    local this_dir=$(basename $(pwd))
-    local mvn_opts=""
-    if [ "$NETWORK" = "false" ]; then
-        mvn_opts="-o"
+function run_tests {
+    local project="$1"
+    local clojure_jar="$2"
+    local mvn_opts="-q"
+    local rv=""
+
+    if [ ! -z "$clojure_jar" ]; then
+        mvn_opts="${mvn_opts} -Dclojure.jar=${clojure_jar}"
     fi
-    
-    if [ -e project.clj ]; then
-        echodo lein test
-    elif [ -e pom.xml ]; then
-        echodo mvn $mvn_opts test
-    else
-        echo "WARNING: Don't know how to test $this_dir"
+    if [ "$OFFLINE" = "true" ]; then
+        mvn_opts="${mvn_opts} -o"
     fi
+
+    (
+        cd "$WORK_DIR/$project"
+        if [ -e project.clj ]; then
+            cd lib
+            rm -f clojure.jar \
+                clojure-?.?.?.jar \
+                clojure-?.?.?-alpha?.jar \
+                clojure-?.?.?-beta?.jar \
+                clojure-?.?.?-master-SNAPSHOT.jar \
+                clojure-?.?.?-SNAPSHOT.jar
+            ln -s "${clojure_jar}" lib/clojure.jar
+            echodo lein test
+            rv="$?"
+        elif [ -e pom.xml ]; then
+            echodo mvn $mvn_opts test
+            rv="$?"
+        else
+            echo "WARNING: Don't know how to test $project"
+        fi
+    )
+    return $rv
+}
+
+function do_argos_project {
+    local project="$1"
+    update_git "$project"
+    update_deps "$project"
+    run_tests "$project"
 }
